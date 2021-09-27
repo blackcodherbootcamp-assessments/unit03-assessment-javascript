ae87a6fc6a995d07b2ae6e57290477d505a51335
Genyus <genyus@gmail.com>
2021-09-28 00:59:57 +0100

    chore: makes selector more flexible

--- .autograding/__tests__/content.js
+++ .autograding/__tests__/content.js
@@ -6,6 +6,6 @@ const doc = parseHTML(html);
 describe('HTML', () => {
   test('body contains exactly one <div /> element with the class name "timeline"', () => {
     expect(doc.querySelectorAll('body')[0].childNodes.filter(node => 1 === node.nodeType).length).toBe(2);
-    expect(doc.querySelectorAll('body > div.timeline').length).toBe(1);
+    expect(doc.querySelectorAll('body div.timeline').length).toBe(1);
   });
 });
